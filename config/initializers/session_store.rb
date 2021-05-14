88a68b59b1f9b1b577daaabb6bf312b4c9b639a1
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-05-14 12:26:19 -0700

    Differentiate between dev and prod envs for cookie_store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,5 @@
-Rails.application.config.session_store :cookie_store, key: "_authentication", domain: "http://localhost:3000" # API source app, or on Heroku like "https://abc-xyz.herokuapp.com"
+if Rails.env == "production"
+  Rails.application.config.session_store :cookie_store, key: "_authentication", domain: "abc-xyz.herokuapp.com" # update this for API source app production environment host
+else
+  Rails.application.config.session_store :cookie_store, key: "_authentication"
+end
