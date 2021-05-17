9e89ac0f30f40e7c392cc1311e993f9487d30244
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-05-17 15:29:35 -0700

    Remove registration controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   resources :sessions, only: [:create]
-  resources :registrations, only: [:create]
   resources :organizations do
     resources :events
     resources :users
