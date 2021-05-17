881b6bfe5a3635ac9e1433aba5dd7a02c553806e
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-05-17 11:22:44 -0700

    Add foreign key to users for organizations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_17_181506) do
+ActiveRecord::Schema.define(version: 2021_05_17_182038) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,4 +43,5 @@ ActiveRecord::Schema.define(version: 2021_05_17_181506) do
     t.integer "organization_id"
   end
 
+  add_foreign_key "users", "organizations"
 end
