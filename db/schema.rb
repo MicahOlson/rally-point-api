1ed5d51978205be46a9831e3cc86a4e16b224271
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-05-17 11:33:21 -0700

    Add foreign key to events for organizations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_17_182512) do
+ActiveRecord::Schema.define(version: 2021_05_17_182751) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,5 +50,6 @@ ActiveRecord::Schema.define(version: 2021_05_17_182512) do
     t.integer "organization_id"
   end
 
+  add_foreign_key "events", "organizations"
   add_foreign_key "users", "organizations"
 end
